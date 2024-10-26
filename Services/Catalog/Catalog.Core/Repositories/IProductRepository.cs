0c85abacab1d9a3e40ca9bd14e8f45af51fd7cde
youlchu <yyolcu634@gmail.com>
2024-10-27 02:06:08 +0300

    feat: first commit

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Core.Entities;
+//using Catalog.Core.Specs;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        //Task<Pagination<Product>> GetProducts(CatalogSpecParams catalogSpecParams);
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByBrand(string brandName);
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
